f2fb0d6aa11f81da1c16699cd3d8d098f8edf3d3
Mikael Berthe <mikael@lilotux.net>
2017-04-28 22:00:07 +0200

    Bump version, display madon version with madonctl version
    (Updating version to trigger a rebuild with madon 1.1.1...)

--- cmd/version.go
+++ cmd/version.go
@@ -9,16 +9,19 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+
+	"github.com/McKael/madon"
 )
 
 // VERSION of the madonctl application
-var VERSION = "0.3.0"
+var VERSION = "0.3.1"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Display " + AppName + " version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("This is %s version %s\n", AppName, VERSION)
+		fmt.Printf("This is %s version %s (using madon library version %s).\n",
+			AppName, VERSION, madon.MadonVersion)
 	},
 }
 
