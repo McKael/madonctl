82fc62472b0e23222d2ee8c0028b78d2edbd82b3
Mikael Berthe <mikael@lilotux.net>
2017-10-21 19:05:11 +0200

    Change version to 2.0.0-dev

--- cmd/version.go
+++ cmd/version.go
@@ -23,7 +23,7 @@ type madonctlVersion struct {
 }
 
 // VERSION of the madonctl application
-var VERSION = "1.2.1-dev"
+var VERSION = "2.0.0-dev"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
